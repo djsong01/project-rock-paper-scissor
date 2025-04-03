bfb63110735fb3bc07f5e5d0edc87f81521c8789
dj <200647821+djsong01@users.noreply.github.com>
2025-04-03 11:46:41 -0700

    feat: Created buttons to choose Rock, Paper, or Scissor

--- my-java.js
+++ my-java.js
@@ -13,6 +13,7 @@ function getComputerChoice(){
     }
 }
 
+
 function getHumanChoice(){
     let a = prompt("Rock, Paper, or Scissor?");
     let choice = a.charAt(0).toUpperCase() + a.slice(1).toLowerCase();
@@ -25,42 +26,35 @@ function getHumanChoice(){
     }
 }
 
+
 let humanScore = 0;
 let computerScore = 0;
 let rounds = 0;
 
-
 function playRound(humanChoice,computerChoice){
     let hScore = 0;
     let cScore = 0;
-    let roundCount = 0;
+    let roundCount = 1;
 
     if (humanChoice === computerChoice){
         roundCount++;
     }
     else if (humanChoice === "Rock" && computerChoice === "Paper"){
-        cScore++;
-        roundCount++;
     }
     else if (humanChoice === "Rock" && computerChoice === "Scissor"){
         hScore++;
-        roundCount++;
     }
     else if (humanChoice === "Paper" && computerChoice === "Rock"){
         hScore++;
-        roundCount++;
     }
     else if (humanChoice === "Paper" && computerChoice === "Scissor"){
         cScore++;
-        roundCount++;
     }
     else if (humanChoice === "Scissor" && computerChoice === "Rock"){
         cScore++;
-        roundCount++;
     }
     else if (humanChoice === "Scissor" && computerChoice === "Paper"){
         hScore++;
-        roundCount++;
     }
     
     return{
@@ -70,23 +64,71 @@ function playRound(humanChoice,computerChoice){
     };
 }
 
-function playGame(num){
-    while(rounds < num){
-    const human = getHumanChoice();    
-    const comp = getComputerChoice();
-    const roundResult = playRound(human,comp)
+const rock = document.querySelector("#Rock");
+rock.value = "Rock";
+const paper = document.querySelector("#Paper");
+paper.value = "Paper"
+const scissor = document.querySelector("#Scissor")
+scissor.value ="Scissor"
 
+rock.addEventListener(('click'), () => {
+    let comp = getComputerChoice();
+    const roundResult = playRound("Rock",comp);
     humanScore += roundResult.humanScore;
     computerScore += roundResult.computerScore;
     rounds += roundResult.rounds;
-    
+
+    console.log("Round: " + rounds);
+    console.log("Human says " + rock.value);
+    console.log("Computer says " + comp);
+    console.log("Human score: " + humanScore);
+    console.log("Computer score: " + computerScore);
+    });
+
+paper.addEventListener(('click'), () => {
+    let comp = getComputerChoice();
+    const roundResult = playRound("Paper",comp);
+    humanScore += roundResult.humanScore;
+    computerScore += roundResult.computerScore;
+    rounds += roundResult.rounds;
+
+    console.log("Round: " + rounds);
+    console.log("Human says " + paper.value);
+    console.log("Computer says " + comp);
+    console.log("Human score: " + humanScore);
+    console.log("Computer score: " + computerScore);
+    });
+
+scissor.addEventListener(('click'), () => {
+    let comp = getComputerChoice();
+    const roundResult = playRound("Scissor",comp);
+    humanScore += roundResult.humanScore;
+    computerScore += roundResult.computerScore;
+    rounds += roundResult.rounds;
+
+    console.log("Round: " + rounds);
+    console.log("Human says " + scissor.value);
+    console.log("Computer says " + comp);
+    console.log("Human score: " + humanScore);
+    console.log("Computer score: " + computerScore);
+    });
+            
+
+/*
+function playGame(){
+    const human = getHumanChoice();    
+    const comp = getComputerChoice();
+    const roundResult = playRound(human,comp)
+    humanScore += roundResult.humanScore;
+    computerScore += roundResult.computerScore;
+    rounds += roundResult.rounds;
     console.log("Round: " + rounds);
     console.log("Human says " + human);
     console.log("Computer says " + comp);
     console.log("Human score: " + humanScore);
     console.log("Computer score: " + computerScore);
-    }
-    console.log("Gameover");
+    playGame();
+    //console.log("Gameover");
 }
-
-playGame(5);
+playGame();
+*/
